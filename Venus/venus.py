8d17247768f5b10800d45dac99da46229ef1ed31
root <root@dev-zabbix-server-001.(none)>
2016-04-07 17:36:59 +0800

    update venus

--- Venus/venus.py
+++ Venus/venus.py
@@ -1,34 +1,24 @@
 from flask import Flask,request,abort,render_template,make_response,redirect,url_for,session,escape
-import setting
-def init():
-	for i in range(0,len(setting.data)):
-		setting.data[i]["index"]=i
-init()
+
 app = Flask(__name__)
 
 @app.route('/',methods=['GET', 'POST'])
 def root():
 	#abort(404)
 	if request.method == 'GET':
-		return redirect("/index")
-@app.route('/index',methods=['GET', 'POST'])
-def index():
+		return redirect("/home")
+@app.route('/home',methods=['GET', 'POST'])
+def home():
 	if request.method == 'GET':
-		return render_template("index.html",doctors=setting.doctors,NAME=setting.NAME,items=setting.items,abouts=setting.abouts)
-@app.route('/whoami',methods=['GET', 'POST'])
-def whoami():
+		return render_template("home.html")
+@app.route('/device',methods=['GET', 'POST'])
+def device():
 	if request.method == 'GET':
-		return render_template("whoami.html",NAME=setting.NAME,ABOUT=setting.ABOUT)	
-@app.route('/detail',methods=['GET', 'POST'])
-def detail():
+		return render_template("device.html")
+@app.route('/space',methods=['GET', 'POST'])
+def space():
 	if request.method == 'GET':
-		i=int(request.args.get('i'))
-		length=len(setting.data)
-		if i <0:
-			i=length-1
-		elif i >=length:
-			i=0
-		return render_template("detail.html",NAME=setting.NAME,oneData=setting.data[i])				
+		return render_template("space.html")		
 @app.errorhandler(404)
 def page_not_found(error):
 	#return render_template('404.html'), 404
