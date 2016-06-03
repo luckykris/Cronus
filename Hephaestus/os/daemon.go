c1253a0204b36a617fa0e3f0a287dde965e78595
root <root@dev-zabbix-server-001.(none)>
2016-06-03 10:01:23 +0800

    update daemon

--- Hephaestus/os/daemon.go
+++ Hephaestus/os/daemon.go
@@ -7,7 +7,8 @@ func Daemonize(){
         if os.Getppid()!=1{
                 filePath,_:=filepath.Abs(os.Args[0])
                 args:=append([]string{filePath},os.Args[1:]...)
-                os.StartProcess(filePath,args,&os.ProcAttr{Files:[]*os.File{os.Stdin,os.Stdout,os.Stderr}})
+		devNull,_ := os.OpenFile(os.DevNull, os.O_RDWR, 0)
+                os.StartProcess(filePath,args,&os.ProcAttr{Files:[]*os.File{devNull,devNull,devNull}})
 		os.Exit(0)
         }
 }
